b533b6b4eb99a1dad0f08459263d9412df14d802
Derrick Marshall <marshallderrickdnm@gmail.com>
2020-10-19 10:12:50 -0700

    mkdir

--- server/index.js
+++ server/index.js
@@ -3,7 +3,7 @@ const path = require('path');
 const app = express();
 const port = 3000;
 
-app.use('/', express.static(path.join(__dirname, '../client/distribution/')))
+app.use('/', express.static(path.join(__dirname, '../client/dist/')))
 
 app.listen(port, () => {
   console.log(`App listening at http://localhost:${port}`);
